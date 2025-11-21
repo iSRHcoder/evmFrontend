429c5dbe1d95971f1f4a52fc44357180c8ea7c1b
Sunil Hatkadke <s.r.hatkadke@gmail.com>
2025-11-21 23:55:00 +0530

    Update button text in EvmPage component to simplify localization

--- src/components/evmPage.tsx
+++ src/components/evmPage.tsx
@@ -287,7 +287,7 @@ const EvmPage = () => {
                               voted ? "bg-gray-400" : "bg-blue-600"
                             }`}
                           >
-                            बटन दाबा
+                            बटन
                           </button>
                         </div>
                       ) : (
